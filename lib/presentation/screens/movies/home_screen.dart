bce4bf5a0e86744c61bfa18c87a504a8ff6343ee
matuumdq <salinasmateo@outlook.com>
2023-09-07 08:55:26 -0300

    api consumed for upcoming and top rated movies

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -31,46 +31,81 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   void initState() {
     super.initState();
     ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+    ref.read(popularMoviesProvider.notifier).loadNextPage();
+    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
+    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
   }
 
   @override
   Widget build(BuildContext context) {
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
     final moviesSlideshow = ref.watch(moviesSlideshowProvider);
+    final popularMovies = ref.watch(popularMoviesProvider);
+    final upcomingMovies = ref.watch(upcomingMoviesProvider);
+    final topRatedMovies = ref.watch(topRatedMoviesProvider);
 
-    return Column(
-      children: [
-        const CustomAppbar(),
-
-        MoviesSlideshow(movies: moviesSlideshow),
-
-        MovieHorizontalListview(
-          movies: nowPlayingMovies,
-          label: 'En Cines',
-          subLabel: 'Miercoles 5',
-          loadNextPage: () =>
-              ref.read(nowPlayingMoviesProvider.notifier).loadNextPage(),
+    return CustomScrollView(slivers: [
+      const SliverAppBar(
+        floating: true,
+        flexibleSpace: FlexibleSpaceBar(
+          title: CustomAppbar(),
         ),
-        MovieHorizontalListview(
-          movies: nowPlayingMovies,
-          label: 'En Cines',
-          subLabel: 'Miercoles 5',
-          loadNextPage: () =>
-              ref.read(nowPlayingMoviesProvider.notifier).loadNextPage(),
-        ),
+      ),
+      SliverList(
+          delegate: SliverChildBuilderDelegate((context, index) {
+        return Column(
+          children: [
+            // const CustomAppbar(),
 
-        // Expanded(
-        //   child: ListView.builder(
-        //     itemCount: nowPlayingMovies.length,
-        //     itemBuilder: (context, index) {
-        //       final movie = nowPlayingMovies[index];
-        //       return ListTile(
-        //         title: Text(movie.title),
-        //       );
-        //     },
-        //   ),
-        // )
-      ],
-    );
+            MoviesSlideshow(movies: moviesSlideshow),
+
+            MovieHorizontalListview(
+              movies: nowPlayingMovies,
+              label: 'En Cines',
+              subLabel: 'Miercoles 5',
+              loadNextPage: () =>
+                  ref.read(nowPlayingMoviesProvider.notifier).loadNextPage(),
+            ),
+            MovieHorizontalListview(
+              movies: upcomingMovies,
+              label: 'Proximamente',
+              subLabel: 'Este mes',
+              loadNextPage: () =>
+                  ref.read(upcomingMoviesProvider.notifier).loadNextPage(),
+            ),
+
+            MovieHorizontalListview(
+              movies: popularMovies,
+              label: 'Populares',
+              loadNextPage: () =>
+                  ref.read(popularMoviesProvider.notifier).loadNextPage(),
+            ),
+            MovieHorizontalListview(
+              movies: topRatedMovies,
+              label: 'Mejores Clasificadas',
+              subLabel: 'Todo',
+              loadNextPage: () =>
+                  ref.read(topRatedMoviesProvider.notifier).loadNextPage(),
+            ),
+
+            const SizedBox(
+              height: 10,
+            )
+
+            // Expanded(
+            //   child: ListView.builder(
+            //     itemCount: nowPlayingMovies.length,
+            //     itemBuilder: (context, index) {
+            //       final movie = nowPlayingMovies[index];
+            //       return ListTile(
+            //         title: Text(movie.title),
+            //       );
+            //     },
+            //   ),
+            // )
+          ],
+        );
+      }, childCount: 1))
+    ]);
   }
 }
